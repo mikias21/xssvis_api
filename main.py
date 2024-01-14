0d6a2e7b155833e2b3fa027736c74e99c863124b
mikias21 <mikiasmesfinA0123@gmail.com>
2024-01-15 04:14:33 +0800

    Updated requirements.txt

--- main.py
+++ main.py
@@ -33,10 +33,7 @@ app.include_router(router)
 
 def run_server():
     if General.RELEASE.value:
-        if General.DIGITAL_OCEN.value:
-            uvicorn.run(app, host="0.0.0.0", port=8080)
-        else:
-            uvicorn.run(app, host="0.0.0.0", port=8000)
+        uvicorn.run(app, host="0.0.0.0", port=8000)
     else:
         app.debug = True
         uvicorn.run(app, host="127.0.0.1", port=5000)
